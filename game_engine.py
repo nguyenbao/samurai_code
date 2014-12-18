4405617fcd7f05e737eee39a533c3b84a5ae2838
Thien Bao Ngyuen <ThienBao@B3WHWN1.ssr.symantec.com>
2014-12-18 16:36:16 +0800

    Fix bug

--- game_engine.py
+++ game_engine.py
@@ -8,16 +8,18 @@ import os
 import random
 import operator
 import time
+from optparse import OptionParser
 
 class Lord:
 
     def __init__(self, strength):
-        self.real_intimacy      = [0, 0, 0, 0]
-        self.reveal_intimacy    = [0, 0, 0, 0]
-        self.nighttime_visit    = [0, 0, 0, 0]
-        self.strength =  strength
+        self.real_intimacy      = [0, 0, 0, 0] # the real intimacy for each player
+        self.reveal_intimacy    = [0, 0, 0, 0] # the reveal intimacy for each player
+        self.nighttime_visit    = [0, 0, 0, 0] # this is to store visit in previous night turn
+        self.nighttime_intimacy = [0, 0, 0, 0] # this is to store intimacy for night tunr 1 and 2
+        self.strength           =  strength
 
-    def increase_intimacy_degree(self, player_id, daily_or_nightime):
+    def increase_intimacy_degree(self, player_id, daily_or_nightime, turn=0):
         if player_id in range(0, len(self.real_intimacy)):
             if daily_or_nightime == "D":
                 self.real_intimacy[player_id]   += 1
@@ -25,6 +27,8 @@ class Lord:
             else:
                 self.real_intimacy[player_id] += 2
                 self.nighttime_visit[player_id] += 1
+                if turn == 1 or turn == 3:
+                    self.nighttime_intimacy[player_id] += 2
 
     def reset_nighttime_intimacy(self):
         self.nighttime_visit = [0, 0, 0, 0]
@@ -35,6 +39,8 @@ class Lord:
     def provide_military_force(self, player_id):
         max_value = max(self.real_intimacy)
         min_value = min(self.real_intimacy)
+        if max_value == min_value:
+            return 0
         num_max   = self.real_intimacy.count(max_value)
         num_min   = self.real_intimacy.count(min_value)
         if self.real_intimacy[player_id] == max_value:
@@ -47,8 +53,8 @@ class Lord:
     def get_strength(self):
         return self.strength
 
-    def get_reveal_intimacy_str(self, player_id):
-        using_initimacy = self.reveal_intimacy
+    def get_reveal_intimacy_str(self, player_id, turn=0):
+        using_initimacy = self.reveal_intimacy if turn <= 4 else [ i+j for i,j in zip(self.reveal_intimacy, self.nighttime_intimacy) ]
         initimacy = list(map(str,using_initimacy[player_id:] + using_initimacy[:player_id]))
         return " ".join(initimacy)
     
@@ -87,112 +93,143 @@ def create_lord():
 
 
 def main():
-    if len(sys.argv) != 5:
-        print "Usage: game_engine.py [player1] [player2] [player3] [player4]\n"
+    parser = OptionParser("Usage: game_engine.py [player1] [player2] [player3] [player4] <option>")
+    parser.add_option("-m", dest="manually", help="Manually Continue at each turns.", action="store_true")
+    parser.add_option("--sleep", dest="sleep_time", help="When auto runs, the number of seconds to sleep each turn", type=int, default=1)
+    parser.add_option("--time", dest="match_time", help="Number of match to run", default=1, type=int)
+
+    options, args = parser.parse_args()
+    if len(args) != 4:
+        parser.print_help()
         sys.exit(1)
-
-    print "Generating lords...."
-    lords = create_lord()
-    print "Lords generation done."
-    print "Initializing players..."
-    players = list()
-    for i in range(1,len(sys.argv)):
-        player = execute_process(sys.argv[i])
-        if not player:
-            print "Usage: game_engine.py [player1] [player2] [player3] [player4]"
-            sys.exit(1)
-        else:
-            players.append(player)
-    print "Done initializing players."
-
-    # game loop
-    for i in range(len(players)):
-        player = players[i]
-        ready = read_from_process(player)
-        print "AI%d>>%s" % (i, ready)
-        if ready == "READY":
-            pass
-            #print "AI%d>>READY" % i
-        else:
-            pass
-            #print "AI%d>>not READY.Remove it from." %i
     
-    for i in range(len(players)):
-        player = players[i]
-        print "AI%d>>Writing to stdin. Waiting for stdout." % i
-        print "9 4 6"
-        write_to_process("9 4 6", player)
-        print " ".join(["%d" % lord.get_strength() for lord in lords ])
-        write_to_process("%s" % " ".join(["%d" % lord.get_strength() for lord in lords ]), player)
-    
-    for turn in range(9):
-        # day turn
-        if turn % 2 == 0:
-            for i in range(len(players)):
-                player = players[i]
-                print "(Turn %d)AI%d>>Writing to stdin. Waiting for stdout." % (turn+1, i) 
-                
-                print "%d D" % (turn+1)
-                write_to_process("%d D" % (turn+1), player)
-                # reveal intimacy 
-                for lord in lords:
-                    print "%s" % lord.get_reveal_intimacy_str(i)
-                    write_to_process(lord.get_reveal_intimacy_str(i), player)
-                
-                # real intimacy 
-                print "%s" % " ".join( [lord.get_real_intimacy_str(i) for lord in lords] )
-                write_to_process(" ".join( [lord.get_real_intimacy_str(i) for lord in lords] ), player)
-                # nighttime negotiation time
-                print "%s" % " ".join( [lord.get_nighttime_visit_str() for lord in lords] )
-                write_to_process(" ".join( [lord.get_nighttime_visit_str() for lord in lords] ), player)
-                
-                # flush so that the child process can read it 
-                flush_process(player)
-                result = read_from_process(player)
-                lord_idx = list(map(int, result.split()))
-                for idx in lord_idx:
-                    lords[idx].increase_intimacy_degree(i, "D")
-                
-        else:
-        # night turn
-            for lord in lords:
-                lord.reset_nighttime_intimacy()
-            for i in range(len(players)):
-                player = players[i]
-                print "(Turn %d)AI%d>>Writing to stdin" % (turn+1, i) 
-                
-                print "%d N" % (turn+1)
-                write_to_process("%d N" % (turn+1), player)
-                # reveal intimacy 
-                for lord in lords:
-                    print "%s" % lord.get_reveal_intimacy_str(i)
-                    write_to_process(lord.get_reveal_intimacy_str(i), player)
-                
-                # real intimacy 
-                print "%s" % " ".join( [lord.get_real_intimacy_str(i) for lord in lords] )
-                write_to_process(" ".join( [lord.get_real_intimacy_str(i) for lord in lords] ), player)
-                
-                # flush so that the child process can read it 
-                flush_process(player)
-                result = read_from_process(player)
-                lord_idx = list(map(int, result.split()))
-                for idx in lord_idx:
-                    lords[idx].increase_intimacy_degree(i, "N")
+    manually   = options.manually
+    sleep_time = options.sleep_time
+    match_time = options.match_time
 
-        # print result 
-        print "(Turn %d)Result>>" % (turn+1)
-        scores = {}
+    for match_id in range(match_time):
+        print "=============================="
+        print "Starting match %d" % match_id
+        print "Generating lords...."
+        player_mitlitary_force = [0, 0, 0, 0]
+        lords = create_lord()
+        print "Lords generation done."
+        print "Initializing players..."
+        players = list()
+        for i in range(0,len(args)):
+            player = execute_process(args[i])
+            if not player:
+                print "Usage: game_engine.py [player1] [player2] [player3] [player4]"
+                sys.exit(1)
+            else:
+                players.append(player)
+        print "Done initializing players."
+    
+        # game loop
         for i in range(len(players)):
-            score_i = sum([lord.provide_military_force(i) for lord in lords])
-            scores[i] = score_i
-        sorted_items = sorted(scores.items(), key=operator.itemgetter(1), reverse=True)
-        for item in sorted_items:
-            print "Player %d>> score=(%f)" % (item[0], item[1])
+            player = players[i]
+            ready = read_from_process(player)
+            print "AI%d>>%s" % (i, ready)
+            if ready == "READY":
+                pass
+            else:
+                pass
 
-        time.sleep(1)
+        for i in range(len(players)):
+            player = players[i]
+            print "AI%d>>Writing to stdin. Waiting for stdout." % i
+            print "9 4 6"
+            write_to_process("9 4 6", player)
+            print " ".join(["%d" % lord.get_strength() for lord in lords ])
+            write_to_process("%s" % " ".join(["%d" % lord.get_strength() for lord in lords ]), player)
+        
+        for turn in range(9):
+            # day turn
+            players_result = [None, None, None, None]
+            if turn % 2 == 0:
+                for i in range(len(players)):
+                    player = players[i]
+                    print "(Turn %d)AI%d>>Writing to stdin. Waiting for stdout." % (turn+1, i) 
+                    
+                    print "%d D" % (turn+1)
+                    write_to_process("%d D" % (turn+1), player)
+                    # reveal intimacy 
+                    for lord in lords:
+                        print "%s" % lord.get_reveal_intimacy_str(i)
+                        write_to_process(lord.get_reveal_intimacy_str(i), player)
+                    
+                    # real intimacy 
+                    print "%s" % " ".join( [lord.get_real_intimacy_str(i) for lord in lords] )
+                    write_to_process(" ".join( [lord.get_real_intimacy_str(i) for lord in lords] ), player)
+                    # nighttime negotiation time
+                    print "%s" % " ".join( [lord.get_nighttime_visit_str() for lord in lords] )
+                    write_to_process(" ".join( [lord.get_nighttime_visit_str() for lord in lords] ), player)
+                    
+                    # flush so that the child process can read it 
+                    flush_process(player)
+                    result = read_from_process(player)
+                    print "AI%d>>STDOUT: %s" % (i, result)
+                    lord_idx = list(map(int, result.split()))
+                    players_result[i] = lord_idx
+                
+                # Update the result to lord
+                for player_id in range(len(players_result)):
+                    lord_idx = players_result[player_id]
+                    for idx in lord_idx:
+                        lords[idx].increase_intimacy_degree(player_id, "D", turn=turn)
+
+            else: # night turn
+                for lord in lords:
+                    lord.reset_nighttime_intimacy()
+                for i in range(len(players)):
+                    player = players[i]
+                    print "(Turn %d)AI%d>>Writing to stdin. Waiting for output" % (turn+1, i) 
+                
+                    print "%d N" % (turn+1)
+                    write_to_process("%d N" % (turn+1), player)
+                    # reveal intimacy 
+                    for lord in lords:
+                        print "%s" % lord.get_reveal_intimacy_str(i)
+                        write_to_process(lord.get_reveal_intimacy_str(i), player)
+                    
+                    # real intimacy 
+                    print "%s" % " ".join( [lord.get_real_intimacy_str(i) for lord in lords] )
+                    write_to_process(" ".join( [lord.get_real_intimacy_str(i) for lord in lords] ), player)
+                    
+                    # flush so that the child process can read it 
+                    flush_process(player)
+                    result = read_from_process(player)
+                    print "AI%d>>STDOUT: %s" % (i, result)
+                    lord_idx = list(map(int, result.split()))
+                    players_result[i] = lord_idx
+
+                # Update the result to lord
+                for player_id in range(len(players_result)):
+                    lord_idx = players_result[player_id]
+                    for idx in lord_idx:
+                        lords[idx].increase_intimacy_degree(player_id, "N", turn=turn)
+
+            # print result for turn 5 and turn 9
+            if turn == 4 or turn == 8:
+                for i in range(len(players)):
+                    player_mitlitary_force[i] += sum([lord.provide_military_force(i) for lord in lords])
+                print "(Turn %d)Result>>" % (turn+1)
+                scores = {}
+                for i in range(len(players)):
+                    scores[i] = player_mitlitary_force[i]
+                sorted_items = sorted(scores.items(), key=operator.itemgetter(1), reverse=True)
+                for item in sorted_items:
+                    print "Player %d>> score=(%f)" % (item[0], item[1])
     
-    for player in players:
-        close_process(player)
+            if manually:
+                input_from_user = raw_input("Press key for next turn>>")
+            elif sleep_time > 0:
+                time.sleep(sleep_time)
+            
+        # Close PIPE and terminate child process
+        for player in players:
+            close_process(player)
+            player.terminate()
     
 if __name__ == "__main__":
     main()
